b3cc4c8c076ac4dc0b1f3773c602b847f49d4882
Felps <fe.thomaz7@gmail.com>
2022-10-19 21:19:57 -0300

    Instalação do Babel para retrocompatibilidade de browsers antigos

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,27 @@ module.exports = {
   entry: './js/script.js', // arquivo que gera a build
 
   output: {
+    path: path.resolve(__dirname, './dist/'),
     filename: 'main.js', // arquivo gerado à partir do script.js que terá os códigos compactados pelo Webpack.
-    path: path.resolve(__dirname, './dist/')
   },
-  mode: 'production', // valor padrão do modo
+  
+  // mode: 'production', // valor padrão do modo
   // mode: 'development',
   // watch: true // verifica se há alteração nos arquivos
+  module: {
+    rules:[
+      {
+        test: /\.js$/, // o cifrão pega somente o final no caso o ".js"
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+  ],
+  }
 }
-// rodar npx webpack no terminal
+// rodar "npx webpack" no terminal
