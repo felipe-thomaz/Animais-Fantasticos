93191585206dff9137d753691eb7648106a5b1f6
Felps <fe.thomaz7@gmail.com>
2022-10-27 23:08:38 -0300

    Funcionamento refatorado

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -5,7 +5,8 @@ export default class Funcionamento {
   }
 
   dadosFuncionamento() {
-    this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number); // O SPLIT transforma uma String em uma Array. Já o MAP pega cada item da Array e trás uma Array nova, usando o construtor Number ele já retorna em números.
+    // O SPLIT transforma uma String em uma Array. Já o MAP pega cada item da Array e trás uma Array nova, usando o construtor Number ele já retorna em números.
+    this.diasSemana = this.funcionamento.dataset.semana.split(',').map(Number); 
     this.horarioSemana = this.funcionamento.dataset.horario.split(',').map(Number);
   }
 
