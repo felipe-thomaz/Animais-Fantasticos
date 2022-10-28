795c343b5ea69d386bf572d7285676d415fc7679
Felps <fe.thomaz7@gmail.com>
2022-10-27 22:40:58 -0300

    Menu mobile transformado em Class

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from "./modules/modal.js"
 import Tooltip from "./modules/tooltip.js"
 import DropdownMenu from "./modules/dropdown-menu.js"
 //não precisa importar o outsideClick, pois ele já está sendo importado pelo "dropdown-menu.js"
-import initMenuMobile from "./modules/menu-mobile.js"
+import MenuMobile from "./modules/menu-mobile.js"
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js"
 import fetchBitcoin from "./modules/fetch-bitcoin.js"
@@ -26,15 +26,17 @@ modal.init();
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
 
+const dropdownMenu = new DropdownMenu('[data-dropdown]');
+dropdownMenu.init();
+
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
+initFuncionamento();
+
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker','.btc-preco');
 
 const scrollAnima = new AnimacaoScroll('[data-anime="scroll"]', 'ativo');
 scrollAnima.init();
-
-const dropdownMenu = new DropdownMenu('[data-dropdown]');
-dropdownMenu.init();
-
-initMenuMobile();
-initFuncionamento();
